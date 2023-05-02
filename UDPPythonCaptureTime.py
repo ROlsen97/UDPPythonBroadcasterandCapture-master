d91eb18a2f39bbbe7359dd52d03ac091b16ffc7d
Rolsen <Bruger@LAPTOP-LHCL8MS0>
2023-05-02 13:17:52 +0200

    first commit

---
+++ UDPPythonCaptureTime.py
@@ -0,0 +1,24 @@
+from socket import *
+import requests
+import json
+
+serverPort = 7000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+serverAddress = ('', serverPort)
+
+serverSocket.bind(serverAddress)
+print("The server is ready")
+while True:
+    ClockRead, clientAddress = serverSocket.recvfrom(2048)
+    print(ClockRead)
+    ClockReadDecoded = ClockRead.decode()
+    print("Received message:" + ClockReadDecoded)
+    ClockRead={"Clock":f"{ClockReadDecoded}"}
+    print(ClockRead)
+    #ClockReadDe = json.loads(ClockRead)
+    #print(ClockReadDe)
+
+    
+    api_url = "https://localhost:7118/api/ClockReads"
+    request = requests.post(api_url, json=ClockRead,verify=False)
